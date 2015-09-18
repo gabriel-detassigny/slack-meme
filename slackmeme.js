1515399a8d24d3fd2f1dce5b3e8d3886c7f6f616
Gabriel de Tassigny <gabriel@daft.ie>
2015-09-18 12:07:27 +0100

    List memes

--- slackmeme.js
+++ slackmeme.js
@@ -1,4 +1,5 @@
 var request = require('request');
+var https = require('https');
 
 module.exports = function (req, res, next) {
     var botPayload = {
@@ -20,9 +21,46 @@ module.exports = function (req, res, next) {
             }
         });
     } else {
-      return res.status(200).send('No input');
+      list(function (memes) {
+          botPayload.text = '```\nAvailable Memes :\n';
+          for (i = 0; i < memes.length; i++) {
+              botPayload.text += memes[i].name + '\n';
+          }
+          botPayload.text += '```\n';
+          return res.status(200).send(botPayload.text);
+        //   send(botPayload, function (error, status, body) {
+        //     if (error) {
+        //         return next(error);
+        //     } else if (status !== 200) {
+        //         return next(new Error('Incoming WebHook: ' + status + ' ' + body));
+        //     } else {
+        //         return res.status(200).end();
+        //     }
+        //   });
+      });
     }
 };
+
+function list(callback) {
+    var options = {
+        host: 'api.imgflip.com',
+        path: '/get_memes',
+    };
+    https.get(options, function(response) {
+        var body = '';
+
+        response.on('error', function(err) {
+            console.log(err);
+        });
+        response.on('data', function(chunk) {
+            body += chunk;
+        });
+        response.on('end', function() {
+            body = JSON.parse(body);
+            callback(body.data.memes);
+        });
+    });
+}
 
 var parse = function(str) {
     var args = [];
@@ -44,37 +82,18 @@ var parse = function(str) {
     return args;
 }
 
-function meme() {
-
-}
-
 function send(payload, callback) {
     var path = '/T026MTNPZ/B0ATP0X36/pPHyA2JhblufRGRJSiISTtd0';
     var uri = 'https://hooks.slack.com/services' + path;
 
     request({
-    uri: uri,
-    method: 'POST',
-    body: JSON.stringify(payload)
+        uri: uri,
+        method: 'POST',
+        body: JSON.stringify(payload)
     }, function (error, response, body) {
-    if (error) {
-      return callback(error);
-    }
-
-    callback(null, response.statusCode, body);
+        if (error) {
+          return callback(error);
+        }
+        callback(null, response.statusCode, body);
     });
 }
-
-// module.exports = function (req, res, next) {
-//   var userName = req.body.user_name;
-//   var botPayload = {
-//     text : 'Hello, ' + userName + '!'
-//   };
-//
-//   // avoid infinite loop
-//   if (userName !== 'slackbot') {
-//     return res.status(200).json(botPayload);
-//   } else {
-//     return res.status(200).end();
-//   }
-// }
